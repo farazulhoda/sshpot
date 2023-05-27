cac388dff9e1b7e0f6bb91b7acd09f96f212b9a8
Farazul Hoda <farazulhoda@mrb1nary.local>
2023-05-26 21:53:34 -0500

    sshpot final

---
+++ config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define LISTENADDRESS   "0.0.0.0"
+#define DEFAULTPORT     22
+#define RSA_KEYFILE     "/etc/ssh/ssh_host_rsa_key"
+#define LOGFILE         "/var/log/sshpot_auth.log"
+#define DEBUG           0
+
+#endif
